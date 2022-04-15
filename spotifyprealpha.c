927a3493ea89a044a21401000ab128f50716706d
DavyEugenio <davy.eugenio@hotmail.com>
2022-04-15 09:27:51 -0300

    aaaaaaaaaaaaa

--- spotifyprealpha.c
+++ spotifyprealpha.c
@@ -10,7 +10,7 @@ typedef struct
 
 typedef struct
 {
-    char titulo[50];
+    char titulo[100];
     int id, ano, id_artista, duracao;
 } Album;
 
@@ -64,7 +64,8 @@ int remover_artista(int id, RepMusica *rep);
 int remover_album(int id, RepMusica *rep);
 int remover_musica(int id, RepMusica *rep);
 
-void clear() {
+void clear()
+{
     system("cls||clear");
 }
 
@@ -73,13 +74,14 @@ Artista construtor_artista(char *nome, int id)
     Artista temp;
     strcpy(temp.nome, nome);
     temp.id = id;
+    temp.ouvintes = 0;
     return temp;
 }
 
 Album construtor_album(char *titulo, int ano, int id, int id_artista)
 {
     Album temp;
-    
+
     strcpy(temp.titulo, titulo);
     temp.ano = ano;
 
@@ -129,7 +131,8 @@ void registrar_artista(Artista artista, RepMusica *rep)
 int registrar_album(Album album, RepMusica *rep)
 {
     int artista_index = buscar_artista(album.id_artista, rep);
-    if (artista_index == -1) {
+    if (artista_index == -1)
+    {
         return artista_index;
     }
     rep->albuns.albuns = (Album *)realloc(rep->albuns.albuns, (rep->albuns.index + 1) * sizeof(Album));
@@ -146,7 +149,8 @@ int registrar_album(Album album, RepMusica *rep)
 int registrar_musica(Musica musica, RepMusica *rep)
 {
     int album_index = buscar_album(musica.id_album, rep);
-    if (album_index == -1) {
+    if (album_index == -1)
+    {
         return album_index;
     }
     rep->musicas.musicas = (Musica *)realloc(rep->musicas.musicas, (rep->musicas.index + 1) * sizeof(Musica));
@@ -203,27 +207,33 @@ int buscar_musica(int id, RepMusica *rep)
     return index;
 }
 
-int atualizar_artista(Artista artista, RepMusica *rep) {
+int atualizar_artista(Artista artista, RepMusica *rep)
+{
     int index = buscar_artista(artista.id, rep);
-    if (index == -1) {
+    if (index == -1)
+    {
         return 0;
     }
     rep->artistas.artistas[index] = artista;
     return 1;
 }
 
-int atualizar_album(Album album, RepMusica *rep){
+int atualizar_album(Album album, RepMusica *rep)
+{
     int index = buscar_album(album.id, rep);
-    if(index == -1) {
+    if (index == -1)
+    {
         return 0;
     }
     rep->albuns.albuns[index] = album;
     return 1;
 }
 
-int atualizar_musica(Musica musica, RepMusica *rep){
+int atualizar_musica(Musica musica, RepMusica *rep)
+{
     int index = buscar_musica(musica.id, rep), album_index = buscar_album(musica.id_album, rep);
-    if(index == -1) {
+    if (index == -1)
+    {
         return 0;
     }
     rep->albuns.albuns[album_index].duracao += musica.duracao - rep->musicas.musicas[index].duracao;
@@ -232,50 +242,61 @@ int atualizar_musica(Musica musica, RepMusica *rep){
     return 1;
 }
 
-int remover_artista(int id, RepMusica *rep) {
+int remover_artista(int id, RepMusica *rep)
+{
     int i, index = buscar_artista(id, rep);
-    if (index == -1) {
+    if (index == -1)
+    {
         return 0;
     }
-    for (i = index; i < rep->artistas.index-1; i++) {
-        rep->artistas.artistas[i] = rep->artistas.artistas[i+1];
+    for (i = index; i < rep->artistas.index - 1; i++)
+    {
+        rep->artistas.artistas[i] = rep->artistas.artistas[i + 1];
     }
     rep->artistas.index--;
-    rep->artistas.artistas = (Artista*) realloc(rep->artistas.artistas, rep->artistas.index * sizeof(Artista));
+    rep->artistas.artistas = (Artista *)realloc(rep->artistas.artistas, rep->artistas.index * sizeof(Artista));
     return 1;
 }
 
-int remover_album(int id, RepMusica *rep) {
+int remover_album(int id, RepMusica *rep)
+{
     int i, index = buscar_album(id, rep);
-    if (index == -1) {
+    if (index == -1)
+    {
         return 0;
     }
-    for (i = index; i < rep->albuns.index-1; i++) {
-        rep->albuns.albuns[i] = rep->albuns.albuns[i+1];
+    for (i = index; i < rep->albuns.index - 1; i++)
+    {
+        rep->albuns.albuns[i] = rep->albuns.albuns[i + 1];
     }
     rep->albuns.index--;
-    rep->albuns.albuns = (Album*) realloc(rep->albuns.albuns, rep->albuns.index * sizeof(Album));
+    rep->albuns.albuns = (Album *)realloc(rep->albuns.albuns, rep->albuns.index * sizeof(Album));
     return 1;
 }
 
-int remover_musica(int id, RepMusica *rep){
+int remover_musica(int id, RepMusica *rep)
+{
     int i, index = buscar_musica(id, rep);
     int album_index = buscar_album(rep->musicas.musicas[index].id_album, rep);
-    if(index == -1) {
+    if (index == -1)
+    {
         return 0;
     }
     rep->albuns.albuns[album_index].duracao -= rep->musicas.musicas[index].duracao;
-    for(i=index; i<rep->musicas.index-1; i++){
-        rep->musicas.musicas[i] = rep->musicas.musicas[i+1];
+    for (i = index; i < rep->musicas.index - 1; i++)
+    {
+        rep->musicas.musicas[i] = rep->musicas.musicas[i + 1];
     }
     rep->musicas.index--;
-    rep->musicas.musicas = (Musica*) realloc(rep->musicas.musicas, rep->musicas.index * sizeof(Musica));
+    rep->musicas.musicas = (Musica *)realloc(rep->musicas.musicas, rep->musicas.index * sizeof(Musica));
     return 1;
 }
 
-void print_menu(char **menu) {
+void print_menu(char **menu)
+{
     int i;
-    for (i = 0; i < sizeof(menu)/sizeof(menu[0]); i++) {
+    for (i = 0; i < sizeof(menu) / sizeof(menu[0]); i++)
+    {
         printf("%s\n", menu[i]);
     }
 }
@@ -283,9 +304,8 @@ void print_menu(char **menu) {
 int main()
 {
     RepMusica repositorio = construtor_rep();
-    // char menus[][] = {"1. Gerenciar artistas", "2. Gerenciar albuns", "3. Gerenciar musicas"};
-    
-
+    char menus[][23] = {"1. Gerenciar artistas", "2. Gerenciar albuns", "3. Gerenciar musicas", "4. Sair", "Digite a opcao: "};
+    char menuCrud[][20] = {"1. Cadastrar", "2. Buscar", "3. Listar", "4. Atualizar", "5. Remover", "6. Cancelar", "Digite a opcao: "};
     registrar_artista(construtor_artista("JOAO", 0), &repositorio);
     registrar_artista(construtor_artista("JESSIKA", 0), &repositorio);
 
@@ -317,4 +337,263 @@ int main()
     printf("%s\n", repositorio.musicas.musicas[1].genero);
     // printf("%s\n", repositorio.musicas.musicas[0].titulo);
     printf("%s\n", repositorio.musicas.musicas[1].titulo);
+    int op, subop, id, index, ano, id_artista, id_album, duracao, faixa;
+    char nome[50], titulo[100], genero[20];
+    Artista artista;
+    Album album;
+    Musica musica;
+
+    do
+    {
+        int i, j, k;
+        for (i = 0; i < 5; i++)
+        {
+            printf("%s\n", menus[i]);
+        }
+        scanf("%d", &op);
+        if (op != 4)
+        {
+            for (j = 0; j < 7; j++)
+            {
+                printf("%s\n", menuCrud[j]);
+            }
+            scanf("%d", &subop);
+        }
+        switch (op)
+        {
+        case 1:
+            switch (subop)
+            {
+            case 1:
+                printf("Nome: ");
+                scanf(" %[^\n]%*c", nome);
+                registrar_artista(construtor_artista(nome, 0), &repositorio);
+                break;
+            case 2:
+                printf("Id: ");
+                scanf("%d", &id);
+                index = buscar_artista(id, &repositorio);
+                if (index != -1)
+                {
+                    artista = repositorio.artistas.artistas[index];
+                    printf("Nome: %s\n", artista.nome);
+                    printf("Ouvinte: %d\n", artista.ouvintes);
+                }
+                else
+                {
+                    printf("Artista nao encontrado!\n");
+                }
+                break;
+            case 3:
+                printf("---Artistas---\n");
+                for (k = 0; k < repositorio.artistas.index; k++)
+                {
+                    printf("Id: %d\n", repositorio.artistas.artistas[k].id);
+                    printf("Nome: %s\n", repositorio.artistas.artistas[k].nome);
+                    printf("Ouvinte: %d\n", repositorio.artistas.artistas[k].ouvintes);
+                    printf("--------------\n");
+                }
+                break;
+            case 4:
+                printf("Id: ");
+                scanf("%d", &id);
+                index = buscar_artista(id, &repositorio);
+                if (index != -1)
+                {
+                    artista = repositorio.artistas.artistas[index];
+                    printf("Nome: ");
+                    scanf(" %[^\n]%*c", artista.nome);
+                    atualizar_artista(artista, &repositorio);
+                }
+                else
+                {
+                    printf("Artista nao encontrado!\n");
+                }
+                break;
+            case 5:
+                printf("Id: ");
+                scanf("%d", &id);
+                if (!remover_artista(id, &repositorio))
+                {
+                    printf("Artista nao encontrado!\n");
+                }
+                break;
+            case 6:
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                break;
+            }
+            break;
+        case 2:
+            switch (subop)
+            {
+            case 1:
+                printf("Titulo: ");
+                scanf(" %[^\n]%*c", titulo);
+                printf("Ano: ");
+                scanf("%d", &ano);
+                printf("Id do Artista: ");
+                scanf("%d", &id_artista);
+                registrar_album(construtor_album(titulo, ano, 0, id_artista), &repositorio);
+                break;
+            case 2:
+                printf("Id: ");
+                scanf("%d", &id);
+                index = buscar_album(id, &repositorio);
+                if (index != -1)
+                {
+                    album = repositorio.albuns.albuns[index];
+                    printf("Titulo: %s\n", album.titulo);
+                    printf("Ano: %d\n", album.ano);
+                    printf("Duracao: %d\n", album.duracao);
+                    printf("Artista: %s\n", repositorio.artistas.artistas[buscar_artista(album.id_artista, &repositorio)].nome);
+                }
+                else
+                {
+                    printf("Album nao encontrado!\n");
+                }
+                break;
+            case 3:
+                printf("---Albuns---\n");
+                for (k = 0; k < repositorio.albuns.index; k++)
+                {
+                    printf("Id: %d\n", repositorio.albuns.albuns[k].id);
+                    printf("Titulo: %s\n", repositorio.albuns.albuns[k].titulo);
+                    printf("Ano: %d\n", repositorio.albuns.albuns[k].ano);
+                    printf("Duracao: %d\n", repositorio.albuns.albuns[k].duracao);
+                    printf("Artista: %s\n", repositorio.artistas.artistas[buscar_artista(repositorio.albuns.albuns[k].id_artista, &repositorio)].nome);
+                    printf("--------------\n");
+                }
+                break;
+            case 4:
+                printf("Id: ");
+                scanf("%d", &id);
+                index = buscar_album(id, &repositorio);
+                if (index != -1)
+                {
+                    album = repositorio.albuns.albuns[index];
+                    printf("Titulo: ");
+                    scanf(" %[^\n]%*c", album.titulo);
+                    printf("Ano: ");
+                    scanf("%d", &album.ano);
+                    printf("Id do Artista: ");
+                    scanf("%d", &album.id_artista);
+                    atualizar_album(album, &repositorio);
+                }
+                else
+                {
+                    printf("Album nao encontrado!\n");
+                }
+                break;
+            case 5:
+                printf("Id: ");
+                scanf("%d", &id);
+                if (!remover_album(id, &repositorio))
+                {
+                    printf("Album nao encontrado!\n");
+                }
+                break;
+            case 6:
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                break;
+            }
+            break;
+        case 3:
+            switch (subop)
+            {
+            case 1:
+                printf("Titulo: ");
+                scanf(" %[^\n]%*c", titulo);
+                printf("Genero: ");
+                scanf(" %[^\n]%*c", genero);
+                printf("Duracao(min): ");
+                scanf("%d", &duracao);
+                printf("Numero da faixa: ");
+                scanf("%d", &faixa);
+                printf("Id do Album: ");
+                scanf("%d", &id_album);
+                registrar_musica(construtor_musica(titulo, genero, 0, id_album, duracao, faixa), &repositorio);
+                break;
+            case 2:
+                printf("Id: ");
+                scanf("%d", &id);
+                index = buscar_musica(id, &repositorio);
+                if (index != -1)
+                {
+                    musica = repositorio.musicas.musicas[index];
+                    printf("Titulo: %s\n", musica.titulo);
+                    printf("Genero: %s\n", musica.genero);
+                    printf("Duracao: %d\n", musica.duracao);
+                    printf("Numero da faixa: %d\n", musica.faixa);
+                    album = repositorio.albuns.albuns[buscar_album(musica.id_album, &repositorio)];
+                    printf("Album: %s\n", album.titulo);
+                    printf("Artista: %s\n", repositorio.artistas.artistas[buscar_album(album.id_artista, &repositorio)].nome);
+                }
+                else
+                {
+                    printf("Album nao encontrado!\n");
+                }
+                break;
+            case 3:
+                printf("---Musicas---\n");
+                for (k = 0; k < repositorio.musicas.index; k++)
+                {
+                    printf("Id: %d\n", repositorio.musicas.musicas[k].id);
+                    printf("Titulo: %s\n", repositorio.musicas.musicas[k].titulo);
+                    printf("Genero: %s\n", repositorio.musicas.musicas[k].genero);
+                    printf("Duracao: %d\n", repositorio.musicas.musicas[k].duracao);
+                    printf("Numero da faixa: %d\n", repositorio.musicas.musicas[k].faixa);
+                    album = repositorio.albuns.albuns[buscar_album(repositorio.musicas.musicas[k].id_album, &repositorio)];
+                    printf("Album: %s\n", album.titulo);
+                    printf("Artista: %s\n", repositorio.artistas.artistas[buscar_album(album.id_artista, &repositorio)].nome);
+                    printf("--------------\n");
+                }
+                break;
+            case 4:
+                printf("Id: ");
+                scanf("%d", &id);
+                index = buscar_musica(id, &repositorio);
+                if (index != -1)
+                {
+                    musica = repositorio.musicas.musicas[index];
+                    printf("Titulo: ");
+                    scanf(" %[^\n]%*c", musica.titulo);
+                    printf("Genero: ");
+                    scanf(" %[^\n]%*c", musica.genero);
+                    printf("Duracao(min): ");
+                    scanf("%d", &musica.duracao);
+                    printf("Numero da faixa: ");
+                    scanf("%d", &musica.faixa);
+                    atualizar_musica(musica, &repositorio);
+                }
+                else
+                {
+                    printf("Album nao encontrado!\n");
+                }
+                break;
+            case 5:
+                printf("Id: ");
+                scanf("%d", &id);
+                if (!remover_musica(id, &repositorio))
+                {
+                    printf("Album nao encontrado!\n");
+                }
+                break;
+            case 6:
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                break;
+            }
+            break;
+        case 4:
+            break;
+        default:
+            printf("Opcao invalida!\n");
+            break;
+        }
+    } while (op != 4);
 }
